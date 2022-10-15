4160b0bc68903ae3bbf25c1baea322b62e90e1cc
Ryan Arora <arora.ryan@gmail.com>
2022-10-15 03:20:50 -0400

    added tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script lang="ts">
+  import Base from "../layouts/+base.svelte";
+</script>
+
+<Base>
+  <h1 class="text-2xl">Welcome to SvelteKit</h1>
+  <p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  </p>
+</Base>
