ebe017f6a3d96e1f42786733474b3516bef5f50a
Ryan Arora <arora.ryan@gmail.com>
2022-10-14 23:07:11 -0400

    prettier plugin packagejson

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,5 @@
 /** @type {import('prettier').Config} */
 module.exports = {
-  plugins: ["prettier-plugin-svelte"],
+  plugins: ["prettier-plugin-packagejson", "prettier-plugin-svelte"],
   overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
 };
