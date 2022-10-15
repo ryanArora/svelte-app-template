e2d75b5e22a4a93ca1ff0b0435277a735da2ba69
Ryan Arora <arora.ryan@gmail.com>
2022-10-15 02:00:22 -0400

    add typecheck functionality

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,4 @@
-/** @type {import('prettier').Config} */
+/** @type {import("prettier").Config} */
 module.exports = {
   plugins: ["prettier-plugin-packagejson", "prettier-plugin-svelte"],
   overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
