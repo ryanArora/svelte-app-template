0a6ee631c97cdcf85038a675b217e060845a4126
Ryan Arora <arora.ryan@gmail.com>
2022-10-15 02:07:56 -0400

    add prettier plugin to sort imports

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,9 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  plugins: ["prettier-plugin-packagejson", "prettier-plugin-svelte"],
+  plugins: [
+    "prettier-plugin-packagejson",
+    "@trivago/prettier-plugin-sort-imports",
+    "prettier-plugin-svelte",
+  ],
   overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
 };
